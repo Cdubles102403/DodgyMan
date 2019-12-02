78d7c2105a68608d33f0927260e2249b87537316
Cdubles102403 <44614396+Cdubles102403@users.noreply.github.com>
2019-12-02 09:02:53 -0500

    Add files via upload

---
+++ DodgyMan/js.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+
+$(function(){
+//class for player
+class player{
+    //player constructor
+    let x;
+    let y;
+    let speed;
+    let HasPowerUp;
+    constructor(){
+    let x;
+    let y;
+    let speed;
+    let HasPowerUp;
+    }
+}
+});
