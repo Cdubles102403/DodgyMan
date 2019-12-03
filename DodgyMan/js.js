9eaf3b57dc10a0e7f019db59ae1faac20505a3e1
Cdubles102403 <44614396+Cdubles102403@users.noreply.github.com>
2019-12-03 09:02:31 -0500

    Restructure

--- DodgyMan/js.js
+++ DodgyMan/js.js
@@ -1,22 +1,49 @@
+//player class
+class Player{
+    constructor(){
+        this.x=mouseX;
+        this.y=mouseY;
+        this.hasPowerUp = false;
+        this.size = 30;
+    }
+    draw(){
+        circle(mouseX,mouseY,this.size)
+    }
+}
+
+//enemy class
+class Enemy{
+    constructor(){
+       this.x = 0;
+       this.y = 0;
+        this.speed=5;
+        this.size=20;
+    }
+    move(){
+        this.x+=this.speed;
+        this.y+=1;
+        square(this.x, this.y, this.size, 2);
+    }
+}
+//powerUp class
+class Powerup{
+    constructor(){
+        this.x=0;
+        this.y=0;
+        this.type = "string";
+        this.isUsed = false;
+    }
+}
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(1040, 580);
+    background('red');
+    Player = new Player();
+    enemy = new Enemy();
 }
 
 function draw() {
-
-$(function(){
-//class for player
-class player{
-    //player constructor
-    let x;
-    let y;
-    let speed;
-    let HasPowerUp;
-    constructor(){
-    let x;
-    let y;
-    let speed;
-    let HasPowerUp;
-    }
+    background('red');
+    Player.draw();
+    enemy.move()
 }
-});
