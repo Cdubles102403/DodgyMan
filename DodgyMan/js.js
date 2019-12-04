af858163b2b4c496341d00dd221d8246afb88d97
Cdubles102403 <44614396+Cdubles102403@users.noreply.github.com>
2019-12-04 09:03:06 -0500

    changed background and move style

--- DodgyMan/js.js
+++ DodgyMan/js.js
@@ -1,14 +1,15 @@
 //player class
+//player class
 class Player{
     constructor(){
-        this.x=mouseX;
-        this.y=mouseY;
+        this.x=Math.floor(Math.random() * 1000);
+        this.y= Math.floor(Math.random() * 500);
         this.hasPowerUp = false;
         this.size = 30;
+        this.speed = 5;
     }
     draw(){
-      this.x = mouseX;
-      this.y = mouseY;
+
         circle(this.x,this.y,this.size)
     }
 }
@@ -16,8 +17,8 @@ class Player{
 //enemy class
 class Enemy{
     constructor(){
-       this.x = 0;
-       this.y = 0;
+       this.x = Math.floor(Math.random() * 1000);
+       this.y = Math.floor(Math.random() * 500);
         this.speed=5;
         this.size=20;
     }
@@ -26,13 +27,17 @@ class Enemy{
         this.y+=1;
         square(this.x, this.y, this.size, 2);
     }
+    remake(){
+        
+    }
+    
 }
 //powerUp class
 class Powerup{
     constructor(){
-        this.x=0;
+        this.x=30;
         this.color;
-        this.y=0;
+        this.y=30;
         this.type = "string";
         this.picked = false;
         this.isUsed = false;
@@ -61,17 +66,52 @@ class Powerup{
         }
       }
     }
+    draw(){
+        var x = this.x;
+         var y = this.y;
+        triangle(x, y, x+20, y+20, x-20, y+20);
+    }
+    
 }
 
 function setup() {
-  createCanvas(1040, 580);
-    background('red');
+    createCanvas(1040, 580);
+    background('teals');
+     enemys = [];
     Player = new Player();
     enemy = new Enemy();
+    PU = new Powerup();
+    for(var i=0;i<50;i++){
+        enemys[i] = new Enemy();
+    }
+    
 }
-
+    
 function draw() {
-    background('red');
-    Player.draw();
-    enemy.move()
+    background('teal'); // "clears" canvas
+    enemy.move(); 
+    PU.draw();
+    console.log(enemys[2])
+    for(var j = 0; j<enemys.length; j++){
+        enemys[j].move();
+    }
+    //player controls
+    //up
+        if(keyIsDown (38 )){
+       Player.y -=Player.speed;
+       }
+    //down
+     if(keyIsDown (40 )){
+            Player.y += Player.speed;
+       }
+    //left
+     if(keyIsDown (37 )){
+        Player.x -= Player.speed;
+      }
+    //right
+     if(keyIsDown (39 ) ){
+        Player.x += Player.speed;
+    }
+Player.draw();
+
 }
